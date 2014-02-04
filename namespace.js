8f879e533befb074537700c42c3e216936568eeb
Diogo Reus <diogoreus@gmail.com>
2014-02-04 13:18:33 -0200

    Alteração para que namespace objects recebam pelos menos um nivel de objetos filhos com as mesmas configurações

--- namespace.js
+++ namespace.js
@@ -6,51 +6,74 @@
 define(["module"], function(module) {
     return {
         load: function(name, req, onload, config) {
-            var localConfig = module.config();
-            var modules = localConfig[name],
+            var localConfig = module.config(),
+                configObj = localConfig[name],
+                modules = [],
                 path = [name],
-                type = Object.prototype.toString.call(modules),
-                baseURL = type === '[object Object]' ? modules.baseURL : name,
+                type = Object.prototype.toString.call(configObj),
+                // baseURL = type === '[object Object]' ? configObj.baseURL : name,
                 paths = [];
 
-            if (!modules) {
+            if (!configObj) {
                 var err = new Error("Module configuration is missing");
                 err.namespaceModule = name;
                 onload.error(err);
                 return;
             }
 
-            if (type === '[object Object]') {
-                modules = modules.paths;
-            } else if (type === '[object String]') {
-                modules = modules.split(",");
+            var insertPaths = function(modules, baseURL) {
+                for (var i = 0, limit = modules.length; i < limit; i++) {
+                    var index = modules[i].indexOf('!')
+
+                    if (index !== -1) {
+                        var plugin = modules[i].substr(0, index + 1),
+                            moduleString = modules[i].substr(index + 1);
+
+                        paths.push(plugin + baseURL + '/' + moduleString);
+
+                    } else {
+                        paths.push(baseURL + '/' + modules[i]);
+                    }
+                };
             }
 
-            for (var i = 0, limit = modules.length; i < limit; i++) {
-                var index = modules[i].indexOf('!')
-
-                if (index !== -1) {
-                    var plugin = modules[i].substr(0, index + 1),
-                        moduleString = modules[i].substr(index + 1);
-
-                    paths.push(plugin + baseURL + '/' + moduleString);
-
-                } else {
-                    paths.push(baseURL + '/' + modules[i]);
+            if (configObj.baseURL) {
+                modules = configObj.paths;
+                insertPaths(modules, configObj.baseURL);
+            } else {
+                if (type === '[object String]') {
+                    modules = configObj.split(",");
+                    insertPaths(modules, name);
+                } else if (type === '[object Object]') {
+                    for (var key in configObj) {
+                        if (configObj.hasOwnProperty(key)) {
+                            var mods = configObj[key].paths;
+                            for (var i = 0, limit = mods.length; i < limit; i++) {
+                                modules.push(key + "/" + mods[i]);
+                            }
+                            insertPaths(configObj[key].paths, configObj[key].baseURL);
+                        }
+                    }
                 }
+            }
 
-            };
+            // console.log(paths, modules);
 
             req(paths, function() {
                 var namespace = {},
                     args = arguments;
 
                 modules.forEach(function(module, idx) {
-                    var index = module.indexOf('!');
+                    var indexPlugin = module.indexOf('!'),
+                        indexSubMod = module.indexOf('/');
 
-                    if (index !== -1) {
-                        var moduleString = module.substring(index + 1, module.indexOf('.'));
+                    if (indexPlugin !== -1) {
+                        var moduleString = module.substring(indexPlugin + 1, module.indexOf('.'));
                         namespace[moduleString] = args[idx];
+                    } else if (indexSubMod !== -1) {
+                        var subModules = module.split('/');
+                        namespace[subModules[0]] = namespace[subModules[0]] ? namespace[subModules[0]] : {};
+                        namespace[subModules[0]][subModules[1]] = args[idx];
                     } else {
                         namespace[module] = args[idx];
                     }
